3bbf6c4650095fe171bb47e24797f49907feff31
Fojan Babaali <fozhanmail@gmail.com>
2021-06-06 20:04:17 -0700

    Update index.js

--- components/LoginForm/index.js
+++ components/LoginForm/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { login } from "../../lib/auth";
 export const LoginForm = () => {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
@@ -11,21 +12,19 @@ export const LoginForm = () => {
     let pass = e.target.value;
     setPassword(pass);
   };
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
     console.log(`Username: ${username} | Password: ${password}`);
+    const data = await login(username, password);
+    console.log(data.payload);
   };
   // -------------------------------------------*
   return (
     <div>
       <form onSubmit={submitHandler}>
         <input onChange={usernameHandler} type="text" placeholder="Username" />
-        <input
-          onChange={passwordHandler}
-          type="password"
-          placeholder="Password"
-        />
-        <button type="submit">Submit</button>
+        <input onChange={passwordHandler} type="password" placeholder="Password" />
+        <button type="submit">Login</button>
       </form>
     </div>
   );
